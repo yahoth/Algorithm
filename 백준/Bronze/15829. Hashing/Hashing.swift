36a0afc36d5afb90615961abd73bc0fd3a1cb72d
Taehyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-03-07 20:41:15 +0900

    [Bronze II] Title: Hashing, Time: 12 ms, Memory: 79512 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.swift
+++ 백준/Bronze/15829. Hashing/Hashing.swift
@@ -1,12 +1,25 @@
 import Foundation
 
-let length = Int(readLine()!)!
-let str = readLine()!.map{ String($0) }
-var result = 0
-let m = 1234567891
 let alphabet = "abcdefghijklmnopqrstuvwxyz".map{ String($0) }
-for i in 0...(length-1) {
-    result += (alphabet.firstIndex(of: str[i])! + 1) * Int(pow(31.0, Double(i)))
+let m = 1234567891
+
+let length = Int(readLine()!)!
+let str = readLine()!.map{ alphabet.firstIndex(of: String($0))! + 1 }
+
+var result = 0
+
+for i in 0..<length {
+    result += str[i] * pow31(i)
+    result %= m
 }
 
 print(result % m)
+
+func pow31(_ r: Int) -> Int {
+    var result = 1
+    for _ in 0..<r {
+        result *= 31
+        result %= m
+    }
+    return result
+}
